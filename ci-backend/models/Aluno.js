5af468235792296a4b7e201d92d7fd42f6c7a67c
joaodiasrosa <joaodiasrosa08@gmail.com>
2026-05-06 21:09:45 -0300

    Update Aluno.js
    reorganiza estrutura visual do model

--- ci-backend/models/Aluno.js
+++ ci-backend/models/Aluno.js
@@ -1,30 +1,36 @@
 const { DataTypes: Sequelize } = require('sequelize');
 const { sequelize: db } = require('../config/MysqlConnection');
 
-const Aluno = db.define('aluno', {
-    id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    nome: {
-        type: Sequelize.STRING(100),
-        allowNull: false,
-    },
-    
-    email: {
-        type: Sequelize.STRING(100),
-        allowNull: false,
-    },
-    ra: {
-        type: Sequelize.STRING(10),
-        allowNull: false,
+// Modelo da tabela de alunos
+const Aluno = db.define(
+    'aluno', 
+    {
+        id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome: {
+            type: Sequelize.STRING(100),
+            allowNull: false,
+        },
+        
+        email: {
+            type: Sequelize.STRING(100),
+            allowNull: false,
+        },
+        ra: {
+            type: Sequelize.STRING(10),
+            allowNull: false,
+        }
+    }, 
+
+    {
+        tableName: 'alunos',
+        underscored: true,
+        timestamps: true,
     }
-}, {
-    tableName: 'alunos',
-    underscored: true,
-    timestamps: true,
-});
+);
 
 module.exports = { Aluno };
